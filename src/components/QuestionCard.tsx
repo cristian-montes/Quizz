73c64d990007206f969ddfadc79e0a80761eda13
Cristian Montes <cmontesmujica@gmail.com>
2022-01-15 16:17:52 -0700

    basic structure and logic for the questions component

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+type Props = {
+    question: string;
+    answers: string[]; //array of strings
+    callback: any;
+    userAnswer: any; // --> change it later to the array of answers
+    questionNr: any;
+    totalQuestions: number;
+}
+
+const QuestionCard:  React.FC<Props> = ({
+    question, 
+    answers, 
+    callback, 
+    userAnswer, 
+    questionNr,
+    totalQuestions
+}) => {
+    return(
+        <>
+            <div>
+                <p className="number"> Question: {questionNr}/{totalQuestions}</p>
+                <p  dangerouslySetInnerHTML={{__html: question}} />
+            </div>
+            <div>
+                {answers.map( answer => (
+                    <div>
+                        <button disabled={userAnswer} onClick={callback} />
+                        <span dangerouslySetInnerHTML={{__html: answer}} />
+                    </div>
+                ))}
+            </div>
+        </>
+
+    )
+}
+
+export default QuestionCard;
+
+
+
+
+// dangerouslySetInnerHTM --- to set HTML programmatically or from an extrenal source you would have to use it, instead og traditionally innerHTML. 
